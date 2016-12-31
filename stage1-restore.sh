2efdfc155a5c935cef744c1852750b0ad667357a
zms <zszafran@gmail.com>
2016-12-31 17:08:32 -0500

    Update stage1-restore.sh

--- stage1-restore.sh
+++ stage1-restore.sh
@@ -8,7 +8,7 @@ ROOT_MOUNT="/mnt/gentoo"
 BOOT_EFI_DIR="/EFI/GENTOO"
 GIT_REPO="https://github.com/zszafran/gentoo-mba-dotfiles.git"
 REPO_DIR="$ROOT_MOUNT/repo"
-STAGE3_VERSION="20151029"
+STAGE3_VERSION="20161229"
 
 echo -e "\nSelect ROOT device:\n"
 ROOT_DEVICE=$(select_device_uuid)
@@ -20,10 +20,10 @@ mount_root $ROOT_DEVICE $ROOT_MOUNT
 mount_eti_boot $BOOT_DEVICE $ROOT_MOUNT $BOOT_EFI_DIR
 mount_cdrom $ROOT_MOUNT
 
+install_stage3 $STAGE3_VERSION $ROOT_MOUNT
+
 git_init_repo $REPO_DIR $ROOT_MOUNT $GIT_REPO
 git_config_ignore $REPO_DIR
-
-install_stage3 $STAGE3_VERSION $ROOT_MOUNT
 
 mount_devices $ROOT_MOUNT
 
